9acdd8e6c9862cc5bf250fbfa7e9a4197cfde480
tkomolina <tatjana.nikonova1@gmail.com>
2020-01-15 21:10:50 +0200

    Activity02 ZODIAC

--- src/main/java/jtm/extra01/Zodiac.java
+++ src/main/java/jtm/extra01/Zodiac.java
@@ -11,6 +11,32 @@ public class Zodiac {
 	 */
 	public static String getZodiac(int day, int month) {
 		String zodiac = null;
+		
+		if (month == 3 && day >= 21 && day<=31 || month == 4 && day <=20) {
+	         zodiac = "Aries";
+		}else if (month == 4 && day >= 21 && day<=30 || month == 5 && day >=1 && day <= 21) {
+	         zodiac = "Taurus";
+	    } else if (month == 5 && day >= 22 && day<=31 || month == 6 && day >=1 && day <= 21) {
+	         zodiac = "Gemini";
+	    } else if (month == 6 && day >= 22 && day<=30 || month == 7 &&  day >=1 && day <= 22) {
+	        zodiac = "Cancer";
+	    } else if (month == 7 && day >= 23 && day<=31 || month == 8 && day >=1 && day <= 22) {
+	         zodiac = "Leo";
+	    } else if (month == 8 && day >= 23 && day<=31 || month == 9 && day >=1 && day <=23) {
+	         zodiac = "Virgo";
+	    } else if (month == 9 && day >= 24 && day<=30 || month == 10 && day >=1 && day <=23) {
+	         zodiac = "Libra";
+	    } else if (month == 10 && day >= 24 && day<=31 || month == 11 && day >=1 && day <=22) {
+	         zodiac = "Scorpio";
+	    } else if (month == 11 && day >= 23 && day<=30 || month == 12 && day >=1 && day <=21) {
+	         zodiac = "Sagittarius";
+	    } else if (month == 12 && day <= 22 && day<=31 || month == 1 && day >=1 && day <= 20) {
+	        zodiac = "Capricorn";
+	    } else if (month == 1 && day >= 21 && day<=31 || month == 2 && day >=1 && day <= 19) {
+	         zodiac = "Aquarius";
+	    } else if (month == 2 && day >= 20 && day<=29 || month == 3 && day >=1 && day <=20) {
+	         zodiac = "Pisces";
+	    } else zodiac = "Wrong date";
 		// TODO #1: Implement method which return zodiac sign names
 		// As method parameter - day and month;
 		// Look at wikipedia:
@@ -22,7 +48,7 @@ public class Zodiac {
 	public static void main(String[] args) {
 		// HINT: you can use main method to test your getZodiac method with
 		// different parameters
-		System.out.println(getZodiac(1, 1));
+		System.out.println(getZodiac(12, 8));
 	}
 
 }
